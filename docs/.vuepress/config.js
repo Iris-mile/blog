a0849f72af41800f315d5c2606e196daa88456be
Iris-mile <1296820437@qq.com>
2022-08-16 11:03:14 +0800

    新增vssue评论插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,17 +2,27 @@
 const dayjs = require('dayjs');
 module.exports = {
   base:'/blog/',
-  plugins: [
-    [
-      '@vuepress/last-updated',
+  plugins: {
+    '@vuepress/last-updated':
       {
         transformer: (timestamp) => {
           dayjs.locale('zh-cn')
           return dayjs(timestamp).format('YYYY-MM-DD HH:mm')
         }
-      }
-    ]
-  ],
+      },
+      '@vssue/vuepress-plugin-vssue': {
+        // set `platform` rather than `api`
+        platform: 'github-v4',
+  
+        // all other options of Vssue are allowed
+        owner: 'iris-mile',
+        repo: 'NAME_OF_REPO',
+        clientId: '6d3bb59f83ee73fb0d84',
+        clientSecret: '5102a13d042caf8969ec380020f191272bec1bc2',
+        autoCreateIssue:true
+      },
+  },
+
   title:'七天博客',
   head: [
     ['meta', { name: 'keywords', content: 'vue,vue3' }],//配置网站Keywords
